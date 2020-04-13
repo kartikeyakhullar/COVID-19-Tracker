c268c4502ced397dee857282e3d0c5e4bc27ff5e
Kartikeya <kartikeya.knightriders@gmail.com>
2020-04-13 18:14:46 +0530

    Updates

---
+++ precache-manifest.0c58404a1c1d4c5b5f0458719125fe71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d89740c9d90c873b2715147f68d60f0",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "eb27a9911f160109e573",
+    "url": "/COVID-19-Tracker/static/css/main.cdffb6b6.chunk.css"
+  },
+  {
+    "revision": "4547b59ffe870b3548ba",
+    "url": "/COVID-19-Tracker/static/js/2.2efa35ec.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.2efa35ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb27a9911f160109e573",
+    "url": "/COVID-19-Tracker/static/js/main.2e9fa74b.chunk.js"
+  },
+  {
+    "revision": "d9d6631f59788b4b5a88",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.da36c953.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
